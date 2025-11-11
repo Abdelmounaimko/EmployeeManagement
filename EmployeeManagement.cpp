cfad57230a97967daadeb62baaa2dae39609af6e
Kordjani abdelmounaim <kordjaniabdelmounaim@gmail.com>
2025-11-11 18:00:24 +0100

    Add Employee Management System code

---
+++ EmployeeManagement.cpp
@@ -0,0 +1,133 @@
+#include<iostream>
+using namespace std;
+
+typedef struct employes{
+    int EmplID;
+    char name[50];
+    int age ;
+    float salary ;
+    struct employes * next ;
+}employes;
+
+employes *Head = NULL;
+
+void AddEmpl(){
+    employes * Empl = new employes;
+    if(Empl == NULL){
+        cout << "Memory allocation failed" << endl;
+        return;
+}
+    cout << "enter ID of employee " << endl;
+    cin >> Empl->EmplID ;
+    cout << "enter the name of the employee" << endl;
+    cin >> Empl->name ;
+    cout << "enter age" << endl;
+    cin >> Empl->age;
+    cout << "enter the salary" << endl;
+    cin >> Empl->salary;
+    Empl->next = Head;
+    Head = Empl;
+}
+void DeleteEmpl(){
+    int NumDelEmpl ;
+    employes *DeleteEmp = Head;
+    employes *PrevEmp = NULL;
+    cout << "Enter ID of Employee" <<endl ; 
+    cin >> NumDelEmpl ;
+    if(Head != NULL && Head->EmplID == NumDelEmpl){
+        employes *temp = Head;
+        Head = Head->next;
+        delete temp;
+    }
+    while(DeleteEmp != NULL && NumDelEmpl != DeleteEmp->EmplID){
+        PrevEmp = DeleteEmp;
+        DeleteEmp = DeleteEmp->next;
+    }
+    if (DeleteEmp == NULL){
+        cout << "There is no employee with this ID" <<endl;
+        return;
+    }
+    PrevEmp->next = DeleteEmp->next;
+    delete DeleteEmp;
+}
+void SearchEmp(){
+    int SearchByID;
+    employes * Search = Head;
+    if(Head == NULL){
+        cout <<endl;
+        cout << "-----------------" << endl;
+        cout << "the list is empty" << endl;
+        cout << "-----------------" << endl;
+        return;
+    }
+    cout << "enter the ID of The employee: " ;
+    cin >> SearchByID ;
+    while(Search != NULL && Search->EmplID != SearchByID){
+        Search = Search->next;
+    }
+    if(Search == NULL){
+        cout << "There is no employee for this ID" << endl;
+        return;
+    }
+    cout << "EmployeeID: " << Search->EmplID << endl;
+    cout << "Name: " << Search->name <<endl;
+    cout << "Age: " << Search->age <<endl;   
+    cout << "Salary: " << Search->salary <<endl;
+    cout << "---------------"<<endl;
+}
+ void DisplayEmpl(){
+    employes *Disp;
+    Disp = Head ;
+    if (Disp == NULL){
+        cout << "The list is empty" << endl;
+        return;
+    }
+    cout << "Registred employees" << endl;
+    cout << "-------------------" << endl;
+
+    while(Disp != NULL){
+        cout << "EmployeeID: " << Disp->EmplID << endl;
+        cout << "Name: " << Disp->name <<endl;
+        cout << "Age: " << Disp->age <<endl;   
+        cout << "Salary: " << Disp->salary <<endl;
+        cout << "---------------"<<endl;
+        Disp = Disp->next;
+        
+    }
+ }
+
+ int main(){
+    int choice;
+    do{
+        cout << "\n--- Employee Management System ---" << endl;
+        cout << "1. Add Employee" << endl;
+        cout << "2. Delete Employee" << endl;
+        cout << "3. Search Employee" << endl;
+        cout << "4. Display Employees" << endl;
+        cout << "5. Exit Program" << endl;
+        cout << "----------------------------------" << endl;
+        cout << "Enter your choice (1-5): "; 
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            AddEmpl();
+            break;
+        case 2:
+            DeleteEmpl();
+            break;
+        case 3:
+            SearchEmp();
+            break;
+        case 4:
+            DisplayEmpl();
+            break;
+        case 5:
+            cout << "the program exiting ..." <<endl;
+            exit(0);
+        default: cout << "Enter your choice (1-5)" <<endl;
+            break;
+        }
+    }while(choice != 5);
+    return 0;
+ }
